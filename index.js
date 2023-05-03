0c717209bed77b9d06021e9bbce7fdd4966ee0a4
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-05-04 01:43:35 +0600

    load json data

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 const express = require("express");
 const app = express();
 const cors=require("cors")
-const port = 3000;
+const port = 8000;
 app.use(cors())
+const recipesdata=require('./data/RecipesData.json')
+const chefsdata=require('./data/data.json')
 
 app.get("/", (req, res) => {
   res.send("Chefsmate server running...");
 });
+app.get('/recipes',(req,res)=>{
+    res.send(recipesdata);
+})
+app.get('/chefs',(req,res)=>{
+    res.send(chefsdata);
+})
 
 app.listen(port, () => {
   console.log( `Chefsmate server running on port ${port}!`);
